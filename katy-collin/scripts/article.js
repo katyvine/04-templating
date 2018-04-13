4eeac5793fd305e93d72e11e4b7f27737df6f7ae
Collin Hintzke <collinhintzke5@gmail.com>
2018-04-13 15:00:47 -0700

    fixed zombie code

--- katy-collin/scripts/article.js
+++ katy-collin/scripts/article.js
@@ -12,33 +12,20 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
 
   let template = $('#article-template').html();
   let templateToRender = Handlebars.compile(template);
 
-  // REVIEW: If your template will use properties that aren't on the object yet, add them.
-  // Since your template can't hold any JS logic, we need to execute the logic here.
-  // The result is added to the object as a new property, which can then be referenced by key in the template.
-  // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  // REVIEW: The ternary operator above accomplishes this same logic.
-  // if(this.publishedOn) {
-  //   this.publishStatus = `published ${this.daysAgo} days ago`;
-  // } else {
-  //   this.publishStatus = '(draft)';
-  // }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
   return templateToRender(this);
 
 };
 
-// COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// the .sort function is taking more than one parameter. If you take away the parentices around a,b then it would look exactly the same. But the script needs to understand that that is two seperate parameters
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
